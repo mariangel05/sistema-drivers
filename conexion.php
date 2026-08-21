b833fa5491b932497610093cfd9c535179d2eb60
mariangel05 <mariangelgonzalez077@gmail.com>
2026-08-21 10:30:39 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,9 @@
 <?php
-// Reemplaza cada dato con lo que te da Render en tu base de datos
-$host = "dpg-xxxxxxxxx-a.oregon-postgres.render.com"; // En Render: External/Internal Hostname
-$port = "5432"; 
-$dbname = "sistema_drivers"; // En Render: Database
-$user = "sistema_drivers_user"; // En Render: Username
-$password = "oCFFxzpMgdGAMqvND1..."; // Tu contraseña completa
+$host = "dpg-da447mm1egvs73b9jqv0-a.oregon-postgres.render.com";
+$port = "5432";
+$dbname = "sistema_drivers";
+$user = "sistema_drivers_user";
+$password = "oCFFxzpMgdGAMqvND1Q3RVr42surI4kh";
 
 $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
