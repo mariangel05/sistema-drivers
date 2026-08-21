eb56371216c23f19e0c642ea8bb4fd4641a14f9b
mariangel05 <mariangelgonzalez077@gmail.com>
2026-08-21 14:08:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -42,6 +42,7 @@ $resultado = pg_query($conexion, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Repositorio de Drivers</title>
+    <link rel="icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/715/715697.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <style>
