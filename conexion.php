36aae45c94fc32dfcf1a4af50c014af6d716774c
mariangel05 <mariangelgonzalez077@gmail.com>
2026-08-21 10:38:12 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ $host = "dpg-da447mm1egvs73b9jqv0-a.oregon-postgres.render.com";
 $port = "5432";
 $dbname = "sistema_drivers";
 $user = "sistema_drivers_user";
-$password = "oCFFxzpMgdGAMqvND1Q3RVr42surI4kh";
+$password = "oCFfxzpMgdGAMqvNDlQ3RVr42surI4kh";
 
 // Agregamos sslmode=require al final
 $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require";
