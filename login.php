5a81fdbb7378f1f8fe47e52ec2947dfd5059d69a
mariangel05 <mariangelgonzalez077@gmail.com>
2026-08-24 07:51:56 -0400

    Update login.php

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceso Administrador - Drivers Hub</title>
+    <title>Acceso Administrador - Drivers </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <style>
